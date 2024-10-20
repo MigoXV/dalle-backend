9725cc64d043c9b5ac2800dd501aee790cb93322
lzy <unknown@xx.com>
2024-10-20 02:49:58 +0000

    fix

--- app/main.py
+++ app/main.py
@@ -32,10 +32,10 @@ class GenerateImagesRequest(BaseModel):
 
 # 定义生成并保存图像的函数
 async def generate_and_save_image(
-    index: int, generate_image_request: GenerateImagesRequest
+    generate_image_request: GenerateImagesRequest
 ):
     # 创建 OpenAI 客户端对象
-    client = openai.OpenAI(api_key=api_key, base_url=os.getenv("base_url"))
+    client = openai.OpenAI(api_key=api_key, base_url=os.getenv("BASE_URL"))
     try:
         # 调用 OpenAI API 生成图像
         response_image = client.images.generate(
@@ -65,38 +65,27 @@ async def generate_and_save_image(
         )
     except openai.APIError as e:
         # 如果发生错误，返回状态码和错误信息
-        return None, e.http_status, str(e)
+        return None, e.status_code, str(e)
 
 
 @app.post("/images/generations")
 async def dalle3_drawing(request: GenerateImagesRequest):
     num_images = 1
 
-    # 存储生成失败的任务
-    errors = []
-    images_data = []
-    # 逐个生成图像
-    for index in range(num_images):
-        # 获取任务执行结果（状态码和错误信息）
-        image_data, status_code, error_message = await generate_and_save_image(
-            index, request
-        )
-        # 如果状态码不为 200，输出错误信息
-        if status_code != 200:
-            errors.append(f"Failed to generate image {index + 1}: {error_message}")
-        else:
-            # 如果成功，将图像数据添加到列表中
-            images_data.append(image_data)
-
-    if errors:
-        raise HTTPException(status_code=500, detail=errors)
+    # 获取任务执行结果（状态码和错误信息）
+    image_data, status_code, error_message = await generate_and_save_image(
+        request
+    )
+    # 如果状态码不为 200，输出错误信息
+    if status_code != 200:
+        raise HTTPException(status_code=status_code, detail=error_message)
 
     # 根据 OpenAI 的返回格式，返回图像的 URL 和其他相关信息
     response = {
         "created": datetime.datetime.now().isoformat(),
         "data": [
             {"url": image_data["url"], "revised_prompt": image_data["revised_prompt"]}
-            for image_data in images_data
+            [image_data]
         ],
     }
 
@@ -107,6 +96,7 @@ async def set_api_key(new_api_key: str):
     global api_key
     api_key = new_api_key
     return {"message": f"API key set successfully, new key: {api_key}"}
+
 # 启动 FastAPI 应用程序时，可以通过 uvicorn 运行
 # 例如：
 # uvicorn image:app --reload
